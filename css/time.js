d4ba188f75b06f8db8da30e3fa0b250fe7374ae5
fries <79971420+lsynewworld@users.noreply.github.com>
2022-10-09 23:23:34 +0800

    Update time.js

--- css/time.js
+++ css/time.js
@@ -2,11 +2,12 @@ function gettime() {
     var time = new Date();
     var arr = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
     var month = time.getMonth();
+    var day = time.getDate();
     var hour = time.getHours();
     var min = time.getMinutes();
     var sec = time.getSeconds();
-    var madetime = (month + 1 < 10 ? "(0" : "(") + (month + 1) + "月" + time.getDate() + "日 " + (hour < 10 ? "0" : "") + hour + (min < 10 ? ":0" : ":") + min + "获取)";
-    var madetime1 = "" + time.getFullYear() + "-" + (month + 1 < 10 ? "0" : "") + (month + 1) + "-" + (time.getDate() - 1);
+    var madetime = (month + 1 < 10 ? "(0" : "(") + (month + 1) + "月" + (time.getDate() < 10 ? "0" : "") + (day) + "日 " + (hour < 10 ? "0" : "") + hour + (min < 10 ? ":0" : ":") + min + "获取)";
+    var madetime1 = "" + time.getFullYear() + "-" + (month + 1 < 10 ? "0" : "") + (month + 1) + "-" + (time.getDate() < 10 ? "0" : "") + (day - 1) + " 15:26";
 
     document.getElementById("nowtime").innerText = madetime;
     document.getElementById("nowtime1").innerText = madetime1;
